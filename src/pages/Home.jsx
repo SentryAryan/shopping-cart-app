b91b46b41826edbcaee664b81c4f2e2c8c7e404c
SentryAryan <aryansri20011967@gmail.com>
2024-06-26 17:25:55 +0530

    fixed layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     return (
         <div className={`${posts.loading ? "flex  justify-center items-center" :
             "grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-7"}  
-            px-5 py-10 min-h-[100vh] max-w-6xl `}>
+            px-10 py-10 min-h-[100vh] max-w-6xl `}>
             {
                 posts.loading ? <Spinner /> : (
                     posts.items.map((post) => <Product key={post.id} post={post} />)
